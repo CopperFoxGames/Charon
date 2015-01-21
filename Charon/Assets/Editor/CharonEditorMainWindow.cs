2acfe3f98c5fcb1f7a06a71299463ffc6f7aa33b
BuildTools <unconfigured@null.spigotmc.org>
2015-01-21 01:54:00 -0500

    Messed around with the editor a bit more, and removed my old test editors

---
+++ Charon/Assets/Editor/CharonEditorMainWindow.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class CharonEditorMainWindow : EditorWindow {
+
+	public CharonEditorMainWindow() {
+		SceneView.onSceneGUIDelegate -= this.OnSceneGUI;
+		SceneView.onSceneGUIDelegate += this.OnSceneGUI;
+	}
+	//test
+	public static bool menu_minimized = false;
+	
+	static float startWidth = 256;
+	static float startHeight = 512;
+
+	Rect pos = new Rect (8, 32, 256, 256);
+
+
+	
+	[MenuItem("Charon/LevelEditor")]
+	static void ShowWindow ()
+	{
+		GetWindow(typeof(CharonEditorMainWindow), false, "Level Editor", true);
+	}
+	
+	void OnFocus()
+	{
+		SceneView.onSceneGUIDelegate -= this.OnSceneGUI;
+		SceneView.onSceneGUIDelegate += this.OnSceneGUI;
+	}
+	
+	void OnDestroy()
+	{
+		SceneView.onSceneGUIDelegate -= this.OnSceneGUI;
+	}
+	
+	void OnSceneGUI(SceneView sceneView)
+	{
+		Handles.BeginGUI();
+
+		if(menu_minimized==false) {
+			pos.width = startWidth;
+			pos.height = startHeight;
+		}else{
+			pos.width = 100;
+			pos.height = 20;
+		}
+
+		pos = GUI.Window (0, pos, windowFunction, "");
+		Handles.EndGUI();
+		
+	}
+
+
+	void windowFunction(int windowID) {
+		GUI.DragWindow ();
+	}
+}
--- Charon/Assets/Editor/Util/RatersEditorUtil.cs
+++ Charon/Assets/Editor/Util/RatersEditorUtil.cs
@@ -4,18 +4,6 @@ using UnityEditor;
 
 public class RatersEditorUtil {
 
-	public static bool drawSplash() {
-		EditorGUILayout.LabelField ("Rater193's Engine");
-		EditorGUILayout.LabelField ("For freinds and team mates only!");
-		EditorGUILayout.LabelField ("All ips will be logged!");
-		EditorGUILayout.Space ();
-		EditorGUILayout.LabelField ("Anyone caught using the engine");
-		EditorGUILayout.LabelField ("without permission, will be");
-		EditorGUILayout.LabelField ("terminated!");
-		EditorGUILayout.Space ();
-		EditorGUILayout.LabelField ("Activated", "True");
-		EditorGUILayout.Space ();
-		return true;
-	}
+
 
 }
